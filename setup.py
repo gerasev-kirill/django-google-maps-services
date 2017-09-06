bc104b9ff786a4d0ecade065de952dec597b31a2
Gerasev Kirill <gerasev.kirill@gmail.com>
2017-09-06 17:19:07 +0300

    [*] version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-google-maps-services',
-    version='0.0.2',
+    version='0.1.0',
     packages=['dj_gmap'],
     include_package_data=True,
     license='BSD License',
